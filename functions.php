f910f83f9dd907e9a8a185501c17674887c642d7
Francisco Mat <francisco@f5sites.com>
2018-02-23 22:43:50 +0000

    startup link shortcode

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+
+add_action( 'init', 'your_theme_init' );
+
+function your_theme_init() {
+	add_shortcode('startups-links', 'startups_link_generate');
+	// Disable nesting check to allow inserted pages within inserted pages.
+	#add_filter('insert_pages_apply_nesting_check','__return_false' );
+}
+
+function startups_link_generate($atts) {
+	#$alink;
+	
+	if(!isset($atts['title']))
+		$atts['title']="Visitar Site do Projeto";
+
+	if(isset($atts['url']))
+	$alink = '<br /><a href="https://'.$atts['url'].'" class="button">'.$atts['title'].'</a> <hr />';
+
+	return $alink.do_shortcode('[insert page=3614]').do_shortcode('[insert page=4634]').do_shortcode('[insert page=5034]');
+}
+
+
+
